8f1e282aab88f7202a7c1e5a39584481dd5f2a86
dyx8686-commits <dyx8686@gmail.com>
2026-05-12 00:23:21 +0300

    Player.java

--- src/main/java/com/game/model/Player.java
+++ src/main/java/com/game/model/Player.java
@@ -5,17 +5,30 @@ import java.util.Map;
 
 public class Player {
 
-    public String name;
-    public String type;
+    private String name;
+    private String type;
 
-    public Map<String, Integer> resources = new HashMap<>();
+    private Map<String, Integer> resources = new HashMap<>();
 
     public Player(String name, String type) {
+
         this.name = name;
         this.type = type;
 
         resources.put("FOOD", 0);
         resources.put("WOOD", 0);
         resources.put("ORE", 0);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public Map<String, Integer> getResources() {
+        return resources;
     }
 }
