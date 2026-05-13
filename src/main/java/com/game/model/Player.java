7ace72e38f507f870c85eb921acbe661777a87da
dyx8686-commits <dyx8686@gmail.com>
2026-05-13 18:20:39 +0300

    Player.java

--- src/main/java/com/game/model/Player.java
+++ src/main/java/com/game/model/Player.java
@@ -17,19 +17,29 @@ public class Player {
 
     public Player(String name, String type) {
 
-        this.name = name;
-        this.type = type;
+    this.name = name;
+    this.type = type;
 
-        // стартовые ресурсы
-        inventory.put("FOOD", 0);
-        inventory.put("WOOD", 0);
-        inventory.put("ORE", 0);
-        inventory.put("GOLD", 0);
-        inventory.put("SEEDS", 5);   // стартовые семена
-        inventory.put("SAPLING", 0); // для лесника
-        inventory.put("ORE_BOX", 0);  // для шахтера
-    }
+    inventory.put("FOOD", 0);
+    inventory.put("WOOD", 0);
+    inventory.put("ORE", 0);
+    inventory.put("GOLD", 0);
+    inventory.put("SEEDS", 5);
 
+    // 🌾 FARMER SEEDS
+    inventory.put("WHEAT_SEEDS", 0);
+    inventory.put("CORN_SEEDS", 0);
+    inventory.put("POTATO_SEEDS", 0);
+
+    // 🌲 WOODCUTTER
+    inventory.put("OAK_SAPLING", 0);
+    inventory.put("PINE_SAPLING", 0);
+
+    // ⛏ MINER
+    inventory.put("COPPER_ORE", 0);
+    inventory.put("IRON_ORE", 0);
+    inventory.put("GOLD_ORE_BOX", 0);
+}
     // ===== BASIC INFO =====
 
     public String getName() {
