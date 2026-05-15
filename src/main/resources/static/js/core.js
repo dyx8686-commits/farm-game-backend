bb79d4746fb744ca9d476c6582fd40736c78c518
dyx8686-commits <dyx8686@gmail.com>
2026-05-15 19:11:21 +0300

    Update core.js

--- src/main/resources/static/js/core.js
+++ src/main/resources/static/js/core.js
@@ -52,13 +52,8 @@ console.log(inv);
 }
 renderInventory(inv);
 renderWorld(player);
-    document.getElementById("farmerObjects").innerHTML = `
-    <div class="shop" onclick="openShop()"></div>
-    <div id="player"></div>
-    <div id="farmerPlants"></div>
-    <div id="woodTrees"></div>
-    <div id="minerStones"></div>
-`;
+    const playerEl = document.getElementById("player");
+if (!playerEl) return;
 updatePlayer();
 }
 
