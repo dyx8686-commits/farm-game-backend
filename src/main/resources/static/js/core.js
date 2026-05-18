256dd7b25cd79673c1831145e113ee8d80f833b5
dyx8686-commits <dyx8686@gmail.com>
2026-05-18 17:26:14 +0300

    Update core.js

--- src/main/resources/static/js/core.js
+++ src/main/resources/static/js/core.js
@@ -1,3 +1,4 @@
+console.log("CORE JS LOADED");
 const BASE = "https://farm-game-backend-production.up.railway.app";
 let playerName = new URLSearchParams(window.location.search).get("name");
 if (!playerName) {
@@ -123,6 +124,7 @@ function onCellClick(x, y, cell) {
 }
 
 // ================= PLOTS (CSS GRID) =================
+console.log("CREATE PLOTS CALLED");
 function createPlots(playerType) {
 
     console.log("CREATE PLOTS CALLED", playerType);
